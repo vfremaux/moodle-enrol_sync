7cab0185b3d10b96ca1b966811b2c1e8de785623
Valery Fremaux <valery.fremaux@gmail.com>
2024-08-21 21:58:33 +0200

    fix tests and externallib

--- tests/enrol_sync_external_test.php
+++ tests/enrol_sync_external_test.php
@@ -27,13 +27,17 @@ namespace enrol_sync;
 defined('MOODLE_INTERNAL') || die();
 
 use externallib_advanced_testcase;
+use StdClass;
+use moodle_exception;
+use enrol_sync_external;
+use external_api;
 
 global $CFG;
 
 require_once($CFG->dirroot . '/webservice/tests/helpers.php');
 
 /**
- * Guest enrolment external functions tests
+ * Sync enrolment external functions tests
  *
  * @package    enrol_sync
  * @category   external
@@ -41,7 +45,7 @@ require_once($CFG->dirroot . '/webservice/tests/helpers.php');
  * @copyright  2023 Valery Fremaux (https://www.activeprolearn.com)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class enrol_guest_external_test extends externallib_advanced_testcase {
+class enrol_sync_external_test extends externallib_advanced_testcase {
 
     /**
      * Test get_instance_info
@@ -52,7 +56,7 @@ class enrol_guest_external_test extends externallib_advanced_testcase {
 
         $this->resetAfterTest(true);
 
-        // Check if guest enrolment plugin is enabled.
+        // Check if sync enrolment plugin is enabled.
         $syncplugin = enrol_get_plugin('sync');
         $this->assertNotEmpty($syncplugin);
 
@@ -72,22 +76,22 @@ class enrol_guest_external_test extends externallib_advanced_testcase {
             'roleid' => $studentrole->id]);
 
         $this->setAdminUser();
-        $result = enrol_guest_external::get_instance_info($instance);
-        $result = external_api::clean_returnvalue(enrol_guest_external::get_instance_info_returns(), $result);
+        $result = enrol_sync_external::get_instance_info($instance);
+        $result = external_api::clean_returnvalue(enrol_sync_external::get_instance_info_returns(), $result);
 
         $this->assertEquals($instance, $result['instanceinfo']['id']);
         $this->assertEquals($course->id, $result['instanceinfo']['courseid']);
-        $this->assertEquals('guest', $result['instanceinfo']['type']);
+        $this->assertEquals('sync', $result['instanceinfo']['type']);
         $this->assertEquals('Test instance', $result['instanceinfo']['name']);
         $this->assertTrue($result['instanceinfo']['status']);
 
         $DB->set_field('enrol', 'status', ENROL_INSTANCE_DISABLED, ['id' => $instance]);
 
-        $result = enrol_guest_external::get_instance_info($instance);
-        $result = external_api::clean_returnvalue(enrol_guest_external::get_instance_info_returns(), $result);
+        $result = enrol_sync_external::get_instance_info($instance);
+        $result = external_api::clean_returnvalue(enrol_sync_external::get_instance_info_returns(), $result);
         $this->assertEquals($instance, $result['instanceinfo']['id']);
         $this->assertEquals($course->id, $result['instanceinfo']['courseid']);
-        $this->assertEquals('guest', $result['instanceinfo']['type']);
+        $this->assertEquals('sync', $result['instanceinfo']['type']);
         $this->assertEquals('Test instance', $result['instanceinfo']['name']);
         $this->assertFalse($result['instanceinfo']['status']);
 
@@ -97,7 +101,7 @@ class enrol_guest_external_test extends externallib_advanced_testcase {
         $user = self::getDataGenerator()->create_user();
         $this->setUser($user);
         try {
-            enrol_guest_external::get_instance_info($instance);
+            enrol_sync_external::get_instance_info($instance);
         } catch (moodle_exception $e) {
             $this->assertEquals('coursehidden', $e->errorcode);
         }
@@ -105,12 +109,12 @@ class enrol_guest_external_test extends externallib_advanced_testcase {
         // Student user.
         $DB->set_field('course', 'visible', 1, ['id' => $course->id]);
         $this->setUser($student);
-        $result = enrol_guest_external::get_instance_info($instance);
-        $result = external_api::clean_returnvalue(enrol_guest_external::get_instance_info_returns(), $result);
+        $result = enrol_sync_external::get_instance_info($instance);
+        $result = external_api::clean_returnvalue(enrol_sync_external::get_instance_info_returns(), $result);
 
         $this->assertEquals($instance, $result['instanceinfo']['id']);
         $this->assertEquals($course->id, $result['instanceinfo']['courseid']);
-        $this->assertEquals('guest', $result['instanceinfo']['type']);
+        $this->assertEquals('sync', $result['instanceinfo']['type']);
         $this->assertEquals('Test instance', $result['instanceinfo']['name']);
         $this->assertTrue($result['instanceinfo']['status']);
     }
